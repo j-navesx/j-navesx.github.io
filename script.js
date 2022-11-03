76b441be6c03ef399495cce2517615b52ab249b4
j-navesx <j.naves@campus.fct.unl.pt>
2022-11-03 20:45:59 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
+<<<<<<< Updated upstream
 console.log(document.URL);
 
+=======
+/**
+ * Handle submit events
+ * @param  {Event} event The event object
+ */
+ function handleSubmit (event) {
+
+	// Stop the form from reloading the page
+	event.preventDefault();
+
+	// If there's no file, do nothing
+	if (!file.value.length) return;
+
+	// Create a new FileReader() object
+	let reader = new FileReader();
+
+	// Setup the callback event to run when the file is read
+	reader.onload = logFile;
+
+	// Read the file
+	reader.readAsDataURL(file.files[0]);
+
+}
+
+let app = document.querySelector('#app');
+
+/**
+ * Log the uploaded file to the console
+ * @param {event} Event The file loaded event
+ */
+function logFile (event) {
+	let str = event.target.result;
+	let img = document.createElement('img');
+	img.src = str;
+	app.append(img);
+	console.log(str);
+}
+>>>>>>> Stashed changes
